2f5c09ce6c73cec9e8a93c56eba9a893edbb25eb
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2023-04-22 15:55:25 +0530

    initial project setup

---
+++ iOS/iOS-ChatGPT/iOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChatGPTApp.swift
+//  iOS-ChatGPT
+//
+//  Created by sidkotha on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macOS/macOS-ChatGPT/macOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_ChatGPTApp.swift
+//  macOS-ChatGPT
+//
+//  Created by sidkotha on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct macOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
