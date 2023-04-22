618f49874607f12b1e0c8d6ee78b53fdfe87d423
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2023-04-22 16:26:13 +0530

    added textfield with round border inside VStack

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct MainView: View {
     @State private var chatQueryText: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            TextField("Search", text: $chatQueryText).textFieldStyle(.roundedBorder)
+        }
     }
 }
 
