0ce9cdd072e7c8f886842ba613f38653d7dc788b
siddharth-kothari-spk <siddharth.kothari.spk@gmail.com>
2023-04-22 16:23:50 +0530

    added var chatQueryText

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var chatQueryText: String = ""
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
